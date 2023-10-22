d0ac237d1f8e856b2e28b7f978d42271dbf268a2
AnaSelomit <ana.selomit.tr@gmail.com>
2023-10-21 18:52:33 -0600

    cambio1

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,8 +1,8 @@
-const successAlert = ({ nombre, show, closeSuccess }) => {
+const successAlert = ({ show, closeSuccess }) => {
     return (
         <div className={`content ${show ? "show" : ""}`}>
             <div className="alert-content">
-                <h5>Los datos de {nombre} han sido agregados con éxito</h5>
+                <h5>Los datos de han sido agregados con éxito</h5>
                 <button className="btn-alert" onClick={closeSuccess}>
                     X
                 </button>
